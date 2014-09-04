dc4a89fefad2c9266207d100d51ed29d298242c4
Alexey A. Lukashin <lukash.spb.ru@gmail.com>
2014-09-04 14:54:38 +0400

    very important change

--- src/ru/spbstu/telematics/HelloWorld.java
+++ src/ru/spbstu/telematics/HelloWorld.java
@@ -2,7 +2,7 @@ package ru.spbstu.telematics;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World?");
+		System.out.println("Hello World?!");
 		System.out.println("And now exiting...");
 	}
 }
