35264b13536b67184fdfe9e357fa4595dc77328b
Alexey A. Lukashin <lukash.spb.ru@gmail.com>
2014-09-04 14:45:43 +0400

    added new message

--- src/ru/spbstu/telematics/HelloWorld.java
+++ src/ru/spbstu/telematics/HelloWorld.java
@@ -3,5 +3,6 @@ package ru.spbstu.telematics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("And now exiting...");
 	}
 }
